f8ac4b935d0991ec0e474a48ed9148fb13e233a6
Chantel Chiraswa <ST10497253@rcconnect.edu.za>
2026-05-24 13:01:11 +0200

    Add MemoryStore class for user and memory and recall

--- CyberSecurityChatBot/MemoryStore.cs
+++ CyberSecurityChatBot/MemoryStore.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace CyberSecurityChatBot
 {
-    class MemoryStore
-    {
-    }
+    /*
+     * Memory class
+     * Responsible for storing and recalling user information
+     */
+   public class MemoryStore
+   {
+        //Stores the user's name
+        public string userName { get; set; } = "";
+        
+        // Dictionary used for flexible memory storage
+        private Dictionary<string, string> memory = new Dictionary<string, string>();
+
+        // Store information using a key and a value
+        // Example key = "topic" value = "phishing"
+        public void Store (string key, string value)
+        {
+            memory[key] = value;
+        }
+
+        // Recalls stored information and return emptu string if nothing exists
+        public string Recall(string key)
+        {
+            if (memory.ContainsKey(key))
+            {
+                return memory[key];
+            }
+
+            return "";
+        }
+
+        // creates a personalised sentence
+        public string GetPersonalisedOpener()
+        {
+            if (!string.IsNullOrEmpty(FavouriteTopic))
+            {
+                return "As someone interested in " + FavouriteTopic + ", here is some advice: ";
+            }
+
+            return "";
+        }
+   }
 }
