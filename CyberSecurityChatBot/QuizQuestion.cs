5fc58231b251e318461235476d507cabf5c53953
Chantel Chiraswa <ST10497253@rcconnect.edu.za>
2026-06-24 18:19:16 +0200

    Added QuizManager.cs with 10+ cybersecurity questions,scoring and feedback system

---
+++ CyberSecurityChatBot/QuizQuestion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CyberSecurityChatBot
+{
+    public class QuizQuestion
+    {
+        public string Question { get; set; } = "";
+
+        public List<string> Options { get; set; } = new();
+
+        public string CorrectAnswer { get; set; } = "";
+
+        public string Explanation { get; set; } = "";
+
+        public bool IsTrueFalse { get; set; }
+    }
+    
+}
